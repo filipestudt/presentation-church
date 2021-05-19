5952fd76039b22fc8be68294505e4bf06965c72c
filipestudt <filipe.studt@gmail.com>
2021-05-19 15:13:11 -0300

    Shortcut keys to Bible and socket integration

--- res/pages/bible/src/main.js
+++ res/pages/bible/src/main.js
@@ -1,20 +1,22 @@
 var requestMaker = new RequestMaker(API_BASE_URL);
 var ip = GetURLParameter('ip');
+var socket;
+
+if (ip) {
+    socket = io('http://' + ip + ':' + RECEPTOR_PORT);
+}
 
 (async function() {
     var books = await requestMaker.getBooks();
     for (let book of books) {
         $('#books').append('<option value="'+ book.abbrev +'">' + book.name + '</option>')
     }
+    getNumberOfChapters(books[0].abbrev);
+    loadChapter(books[0].abbrev, 1);
 }())
 
 $('#books').on('change', function(e) {
-    requestMaker.getChapters(e.target.value).then(function(data, err) {
-        $('#chapters').empty();
-        for(let i=1; i <= data.chapters; i++) {
-            $('#chapters').append('<option value="'+ i +'">' + i + '</option>');
-        }
-    })
+    getNumberOfChapters(e.target.value);
     loadChapter(e.target.value, 1);
 })
 
@@ -22,12 +24,42 @@ $('#chapters').on('change', function(e) {
     loadChapter($('#books').val(), e.target.value);       
 })
 
+$(document).on('click', '.verse', function(e) {
+    $('.verse').removeClass('active');
+    $(e.target).addClass('active');
+    socket.emit('slide', e.target.textContent);
+})
+
 function loadChapter(book, chapter) {
     requestMaker.get(book, chapter).then(function(data, err) {
         var count = 0;
         document.getElementById('text').innerHTML = '';
         for (let verse of data) {
-            document.getElementById('text').innerHTML += ++count + '. ' + verse + '<br />';
+            document.getElementById('text').innerHTML += '<span class="verse">' + ++count + '. ' + verse + '</span>';
         }
     }) 
 }
+
+function getNumberOfChapters(book) {
+    requestMaker.getChapters(book).then(function(data, err) {
+        $('#chapters').empty();
+        for(let i=1; i <= data.chapters; i++) {
+            $('#chapters').append('<option value="'+ i +'">' + i + '</option>');
+        }
+    })
+}
+
+document.addEventListener('keyup', function(e) {
+    if(e.key=='ArrowDown' || e.key=='ArrowRight') {
+        let active = $('.verse.active').next();
+        $('.verse').removeClass('active');
+        active.addClass('active');
+        socket.emit('slide', active[0].textContent);
+    }
+    else if(e.key=='ArrowUp' || e.key=='ArrowLeft') {
+        let active = $('.verse.active').prev();
+        $('.verse').removeClass('active');
+        active.addClass('active');
+        socket.emit('slide', active[0].textContent);
+    }
+})
--- res/pages/settings/src/main.js
+++ res/pages/settings/src/main.js
@@ -1,6 +1,11 @@
 var svgOn = `<svg class="hide toggle-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="#7e828b"><path fill="none" d="M0 0h24v24H0z"/><path d="M8 7a5 5 0 1 0 0 10h8a5 5 0 0 0 0-10H8zm0-2h8a7 7 0 0 1 0 14H8A7 7 0 0 1 8 5zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg>`;
 var svgOff = `<svg class="toggle-btn xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" fill="#7e828b"><path fill="none" d="M0 0h24v24H0z"/><path d="M8 5h8a7 7 0 0 1 0 14H8A7 7 0 0 1 8 5zm8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>`
-var data;
+var data, socket;
+
+var ip = GetURLParameter('ip');
+if (ip) {
+    socket = io('http://' + ip + ':' + RECEPTOR_PORT);
+}
 
 try {
     data = JSON.parse(localStorage.getItem('config'));
@@ -25,3 +30,5 @@ $('.btn-toggle').on('click', function(e) {
         e.target.innerHTML = svgOn;
     }
 })
+
+//localStorage.setItem('config',JSON.stringify({fontSize: 33}))
