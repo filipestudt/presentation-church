f29ee60ff8149ca4dca90abcb8becfc2b909fa61
filipestudt <filipestudt@gmail.com>
2021-12-30 16:39:21 -0300

    Create a file for styles to be shared between apps

---
+++ packages/shared/styles/index.ts
@@ -0,0 +1,21 @@
+enum metrics {
+    sm = 8,
+    md = 16,
+    lg = 24,
+    xg = 30,    
+    borderRadius = 20,
+    navBarHeight = 50
+}
+
+enum themeLight {
+    background = '#f3f6fd',
+    contentBackground = '#ffffff'
+}
+
+enum themeDark {
+    background = '#000000',
+    contentBackground = '#ffffff'
+}
+
+
+export { metrics, themeLight, themeDark }
