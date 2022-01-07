034f71d4b54912df7b7c0c58142797c9af906150
filipestudt <filipestudt@gmail.com>
2022-01-07 12:56:56 -0300

    Add theme provider, side bar and routes

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,25 @@
 import React, { useContext } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider } from 'styled-components'
+import ThemeContext from './contexts/ThemeContext';
+import { Wrapper, Row } from './AppStyle';
+import SideBar from './components/SideBar';
+import Home from './pages/Home';
 
 function App() {
+  const { style } = useContext(ThemeContext);
   return (
-    <h1>App</h1>
+    <ThemeProvider theme={style}>
+      <Wrapper>
+        {/* <Header /> */}
+        <Row>
+          <SideBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Row>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
