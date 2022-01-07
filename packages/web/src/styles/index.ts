1acb5f8e20906d166729fec9384299e8fddeb0b7
filipestudt <filipestudt@gmail.com>
2022-01-07 12:53:30 -0300

    Create an interface for the themes

--- packages/web/src/styles/index.ts
+++ packages/web/src/styles/index.ts
@@ -1,27 +1,35 @@
 enum metrics {
-    sm = 8,
-    md = 16,
-    lg = 24,
-    xg = 30,    
-    borderRadius = 20,
-    navBarHeight = 50
+  sm = 8,
+  md = 16,
+  lg = 24,
+  xg = 30,
+  borderRadius = 20,
+  navBarHeight = 50
 }
 
-enum themeLight {
-    background = '#f3f6fd',
-    contentBackground = '#ffffff',
-    iconColor = '#ccc',
-    selectedIconColor = '#fff',
-    selectedIconBackgroundColor = '#888888'
+export interface Theme {
+  background: string;
+  contentBackground: string;
+  iconColor: string;
+  selectedIconColor: string;
+  selectedIconBackgroundColor: string;
 }
 
-enum themeDark {
-    background = '#000000',
-    contentBackground = '#ffffff',
-    iconColor = '#ccc',
-    selectedIconColor = '#fff',
-    selectedIconBackgroundColor = '#888888'
+export const themeLight = <Theme>{
+  background: '#f3f6fd',
+  contentBackground: '#ffffff',
+  iconColor: '#888',
+  selectedIconColor: '#fff',
+  selectedIconBackgroundColor: '#888888'
+}
+
+export const themeDark = <Theme>{
+  background: '#000000',
+  contentBackground: '#ffffff',
+  iconColor: '#ccc',
+  selectedIconColor: '#fff',
+  selectedIconBackgroundColor: '#888888'
 }
 
 
-export { metrics, themeLight, themeDark }
+export { metrics }
