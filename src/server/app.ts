9c257039c9a4abcb041c6349f08bae15d7074f33
filipestudt <filipestudt@gmail.com>
2024-08-13 12:11:43 -0300

    Add cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { PresentationRoute } from './routes/PresentationRoute'
 import { init } from './database'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/presentations', PresentationRoute)
 
